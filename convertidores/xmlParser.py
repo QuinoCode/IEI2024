a22abad0a82cc95e1e12bcc5562033787d1641df
ADRACHE <adrache@etsinf.upv.es>
2024-11-29 09:45:21 +0100

    parser

--- convertidores/xmlParser.py
+++ convertidores/xmlParser.py
@@ -5,6 +5,7 @@ import json
 file = 'datos/monumentos.xml'
 tree = ET.parse(file)
 root = tree.getroot()
+result = []
 
 def typeCheck(tipo):
     answer = None
@@ -21,7 +22,6 @@ def typeCheck(tipo):
     return answer
 
 with open('datos/CLEdata.json', 'w', encoding='utf-8') as f:
-
     for monumento in root.iter('monumento'):
         coords = monumento.find('coordenadas')
         provincia = 'null'
@@ -48,14 +48,18 @@ with open('datos/CLEdata.json', 'w', encoding='utf-8') as f:
         tip = typeCheck(tipo)    
 
         item = {
-            "name" : monumento.find('nombre').text,
-            "type" : tip,
-            "address" : calle,
-            "postal code" : codpost,
-            "longitude" : coords.find('longitud').text,
-            "latitude" : coords.find('latitud').text,
-            "description" : descripcion,
-            "localName" : localidad,
-            "provinceName" : provincia
+            "Monumento" : {
+                "nombre" : monumento.find('nombre').text,
+                "tipo" : tip,
+                "direccion" : calle,
+                "codigo_postal" : codpost,
+                "longitud" : coords.find('longitud').text,
+                "latitud" : coords.find('latitud').text,
+                "descripcion" : descripcion
+            }, 
+            "Localidad" : localidad,
+            "Provincia" : provincia
         }
-        json.dump(item, f, ensure_ascii=False, indent=4)
+        result.append(item)
+
+    json.dump(result, f, ensure_ascii=False, indent=4)
