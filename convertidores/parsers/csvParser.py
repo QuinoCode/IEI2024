aeb479d4182249418bc67ff79069c664b3bb956c
QuinoCode <joaquinangelvalienteruiz@gmail.com>
2024-12-25 12:04:32 +0100

    Revert "Update csvParser.py"
    This reverts commit f1cbc7823d107b4367c0422ae010026396d951be.

--- convertidores/parsers/csvParser.py
+++ convertidores/parsers/csvParser.py
@@ -164,9 +164,10 @@ def obtainCoordenatesFromScrapper(data):
     return data
 
 def obtainPostalCodeAddress(data):
+    api_key = '0de8b6c75c6048a382e50ff276c6ba90'
     for wrapper in data:
         monument = wrapper["Monumento"]
-        monument["direccion"], monument["codigo_postal"] = direccion_codigo_postal(monument["latitud"], monument["longitud"])
+        monument["direccion"], monument["codigo_postal"] = direccion_codigo_postal(monument["latitud"], monument["longitud"], api_key)
     return data
 
 def obtainValidatedCodePostal(data):
