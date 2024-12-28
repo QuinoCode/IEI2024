debd556c395b8bf9d00a31a7b28ab0afc799eb7f
QuinoCode <joaquinangelvalienteruiz@gmail.com>
2024-12-28 10:41:51 +0100

    Added the structure and a few guidelines on how to develop the API's

---
+++ APIs/busqueda/busqueda_controller.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request
+
+api = Flask(__name__)
+
+# Metodo post de ejemplo para que copieis la estructura de como funciona 
+@api.post("/prueba") #URL que "escucha"
+def metodo_post_ejemplo():
+    # Cómo es un método post recibe un JSON
+    data = request.get_json() #recuperamos el JSON
+    # El json es así { "name": "Nombre introducido por el usuario"}
+    name = data.get('name')
+    print(name) #Para que lo muestre por consola
+    return jsonify({"received_name": name}), 200 
+    #Cuando devolvemos la respuesta casi siempre tiene que ser un json, usamos jsonify sobre un mapa de python con formato de JSON (casi son lo mismo)
+"""
+Además de devolver el json devolvemos el status code de HTTP
+------------------------------------------------------------
+200 OK – Petición correcta.
+201 Created - Recurso creado satisfactoriamente.
+400 Bad Request - El cliente envio una petición incorrecta
+401 Unauthorized - El cliente no pasó el proceso de autenticación (no vamos a tener así que nos da igual)
+403 Forbidden - El cliente no tiene permitido el acceso a ese recurso/endpoint (si está autenticado pero con ese nivel no puede acceder).
+404 Not Found - El recurso solicitado no existe.
+500 Internal Server Error - El procesado interno de los datos ha fallado (una excepción o algo rompe el programa)
+"""
+@api.post("/buscar")
+def buscar_monumento():
+    diccionario_respuesta = None
+    data = request.get_json()
+    localidad = data.get('localidad')
+    codigo_postal = data.get('codigo_postal')
+    provincia = data.get('provincia')
+    tipo = data.get('tipo')
+    # TODO: procesar la query internamente con una función del estilo
+    # diccionario_respuesta =  busca_monumento_en_base_datos(localidad, codigo_postal, provincia, tipo) #importado base de datos
+    if not  diccionario_respuesta:
+        return jsonify({"error": "No hubo respuesta de la base de datos"}), 404
+    return jsonify(diccionario_respuesta), 200
+
+# It's a post method since it has to wrapp the data inputted by the user in a json and get methods don't allow for a body
+if __name__ == '__main__':
+    api.run(debug=True, port=5000)
---
+++ APIs/wrapperCLE/wrapperCLE_controller.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+
+api = Flask(__name__)
+
+# Metodo post de ejemplo para que copieis la estructura de como funciona 
+@api.post("/prueba") #URL que "escucha"
+def metodo_post_ejemplo():
+    # Cómo es un método post recibe un JSON
+    data = request.get_json() #recuperamos el JSON
+    # El json es así { "name": "Nombre introducido por el usuario"}
+    name = data.get('name')
+    print(name) #Para que lo muestre por consola
+    return jsonify({"received_name": name}), 200 
+    #Cuando devolvemos la respuesta casi siempre tiene que ser un json, usamos jsonify sobre un mapa de python con formato de JSON (casi son lo mismo)
+"""
+Además de devolver el json devolvemos el status code de HTTP
+------------------------------------------------------------
+200 OK – Petición correcta.
+201 Created - Recurso creado satisfactoriamente.
+400 Bad Request - El cliente envio una petición incorrecta
+401 Unauthorized - El cliente no pasó el proceso de autenticación (no vamos a tener así que nos da igual)
+403 Forbidden - El cliente no tiene permitido el acceso a ese recurso/endpoint (si está autenticado pero con ese nivel no puede acceder).
+404 Not Found - El recurso solicitado no existe.
+500 Internal Server Error - El procesado interno de los datos ha fallado (una excepción o algo rompe el programa)
+"""
+if __name__ == '__main__':
+    api.run(debug=True, port=5003)
